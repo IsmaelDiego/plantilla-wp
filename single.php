36dd405c6d7b99a3bb46ffc7019ef8a180f52d85
Ismael Diego <ismaeldiegoqm@gmail.com>
2024-07-03 13:08:59 -0500

    Update single.php

--- single.php
+++ single.php
@@ -14,17 +14,15 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-4 text-center">
                                 <?php if ( has_post_thumbnail() ) {
-                                                    the_post_thumbnail('thumbnail', array( 'class' => 'card-image-top alignleft' ));
+                                                    the_post_thumbnail( array( 500, 500  ) );
                                                 }
                                                 ?>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-8">
                                     <h5 class="card-title"><?php the_title();?></h5>
                                     <p class="card-text"><?php the_content();?></p>
-                                    <p class="card-text">PRECIO: $50</p>
-                                    <p class="card-text">PRECIO DE OFERTA: $40</p>
                                     <div class="input-group input-group-sm mb-3">
                                         <button class="btn btn-danger" type="button" id="minusBtn" onclick="cambiar_cantidad('resta')">-</button>
                                         <input type="text" class="form-control text-center" minlength="0" id="cantidad" value="1" placeholder="1" aria-label="Quantity" aria-describedby="basic-addon2">
@@ -32,16 +30,32 @@
                                     </div>
                                     <p id="totalPrice" class="mb-0">Total Price: $50</p>
                                     <a href="#" class="btn btn-primary btn-sm">Add to Cart</a>
+                                    <hr>
+                                    <div class="tags row">      
+                                        <div class="col-6"><?php the_tags( 'Etiquetas:&nbsp; &nbsp;', '&nbsp;-&nbsp;', '<br />' ); ?> </div>
+                                        <div class="col-6"><p>Categoria: <?php the_category( '&nbsp;-&nbsp;' ); ?></p></div>
+                                    </div>
                                 </div>
-                            </div>
+                                
+
+                            </div> 
                             <?php
                                 endwhile;
                                         else :
                                             _e( 'Lo siento no la categoria seleccionada', 'textdomain' );
                                         endif; ?>
-                            <hr>
-                            <p class="small text-muted">Published: June 27, 2024 | 10:00 AM</p>
+                            
                         </div>
+                        <div class="card-footer">
+                        <div class="row">
+                                <div class="col-6">
+                        <p class="small text-muted text-left"><b>Publicado por:&nbsp</b> <?php the_author(); ?></p>
+                                </div>
+                                <div class="col-6">
+                            <p class="small text-muted text-right"><b>Fecha de publicación:</b>&nbsp <?php the_date(); ?> | <?php the_time(); ?></p>
+                                </div>
+                            </div>
+                            </div>
                     </div>
                 </div>
             </div><!-- end row -->
